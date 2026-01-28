79a5c4edef81eef29742eeee3d8d7ecc986c6fbd
Devanshi-Mahto <mahtodevanshi@gmail.com>
2026-01-28 22:25:48 +0530

    Added utxo manager

---
+++ src/utxo_manager.py
@@ -0,0 +1,51 @@
+class UTXOManager :
+ # Store UTXOs as dictionary : (tx_id , index ) -> (amount , owner )
+    def __init__ ( self ) :
+       self.utxo_set = {}
+
+    def add_utxo ( self , tx_id : str , index : int , amount : float , owner :str ) :
+       """ Add a new UTXO to the set ."""
+       self.utxo_set[(tx_id,index)]={
+          "amount":amount,
+          "owner":owner
+       }
+
+    def remove_utxo ( self , tx_id : str , index : int ) :
+       """ Remove a UTXO ( when spent )."""
+
+       if (tx_id,index) in self.utxo_set:
+         del self.utxo_set[(tx_id,index)]
+       
+
+    def get_balance ( self , owner : str ) -> float :
+       """ Calculate total balance for an address ."""
+       balance =0.0
+       for utxo in self.utxo_se.values():
+          if utxo["owner"]==owner:
+             balance +=utxo["amount"]
+       return balance
+
+    def exists ( self , tx_id : str , index : int ) -> bool :
+       """ Check if UTXO exists and is unspent ."""
+       return (tx_id,index) in self.utxo_set
+      
+
+    def get_utxos_for_owner ( self , owner : str ) -> list :
+      """ Get all UTXOs owned by an address ."""
+      result=[]
+      for(tx_id,index), utxo in self.utxo_set.items():
+         if utxo["owner"]==owner:result.append((tx_id,index,utxo["amount"]))
+      return result
+    
+    def __str__(self):
+       """Print the UTXO set"""
+
+       if not self.uyxo_set:
+           return "UTXO set is empty."
+       lines=["Currect UTXO set"]
+       
+       for(tx_id,index),utxo in self.utxo-set.items():
+          lines.append(
+             f"({tx_id},{index}) ->{utxo['amount']} BTC | Owner : {utxo['owner']}"
+          )
+       return "\n".join(lines)
